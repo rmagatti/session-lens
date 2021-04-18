858a1f4d2e14f7420ce6ae605ac9ed2369a0eb33
Ronnie Magatti <ronniemagatti@gmail.com>
2021-04-17 21:13:35 -0600

    Fix previewer issues

--- lua/telescope/_extensions/session-lens/main.lua
+++ lua/telescope/_extensions/session-lens/main.lua
@@ -1,6 +1,7 @@
 local Lib = require('telescope._extensions.session-lens.session-lens-library')
 local AutoSession = require('auto-session')
 local SessionLensActions = require("telescope._extensions.session-lens.session-lens-actions")
+local conf = require('telescope.config').values
 
 ----------- Setup ----------
 local SessionLens = {
@@ -8,8 +9,9 @@ local SessionLens = {
 }
 
 local defaultConf = {
-  theme_conf = { winblend = 10, border = true, previewer = false },
-  shorten_path = true
+  theme_conf = { winblend = 10, border = true },
+  shorten_path = true,
+  previewer = false
 }
 
 -- Set default config on plugin load
@@ -31,6 +33,16 @@ SessionLens.search_session = function(custom_opts)
 
   local shorten_path = custom_opts.shorten_path
   local theme_opts = themes.get_dropdown(custom_opts.theme_conf)
+  custom_opts["shorten_path"] = nil
+  custom_opts["theme_conf"] = nil
+
+  -- Use default previewer config by setting the value to nil if some sets previewer to true in the custom config.
+  -- Passing in the boolean value errors out in the telescope code with the picker trying to index a boolean instead of a table.
+  -- This fixes it but also allows for someone to pass in a table with the actual preview configs if they want to.
+  if custom_opts.previewer ~= false and custom_opts.previewer == true then
+    custom_opts["previewer"] = nil
+  end
+
   local opts = {
     prompt_title = 'Sessions',
     entry_maker = Lib.make_entry.gen_from_file({cwd = cwd, shorten_path = shorten_path}),
