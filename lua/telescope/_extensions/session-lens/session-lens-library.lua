6d5858cc6217e94ae180fc5177a6e1dd036082aa
Ronnie Magatti <ronniemagatti@gmail.com>
2021-07-15 02:20:04 -0600

    Some cleanup and temporarily break the custom maker.
    I need to figure out a more reliable way of having the custom display
    working, for now showing the session files full path will have to do.
    
    Closes #10
    
    Fixing this creates a regression with the path shortening. #11

--- lua/telescope/_extensions/session-lens/session-lens-library.lua
+++ lua/telescope/_extensions/session-lens/session-lens-library.lua
@@ -1,5 +1,5 @@
-local path = require('telescope.path')
-local utils = require('telescope.utils')
+-- local path = require('plenary.path')
+-- local utils = require('telescope.utils')
 
 local Config = {}
 local Lib = {
@@ -72,64 +72,64 @@ end
 
 
 -- ==================== SessionLens ==========================
-do
-  local lookup_keys = {
-    ordinal = 1,
-    value = 1,
-    filename = 1,
-    cwd = 2,
-  }
+-- do
+--   local lookup_keys = {
+--     ordinal = 1,
+--     value = 1,
+--     filename = 1,
+--     cwd = 2,
+--   }
 
-  function Lib.make_entry.gen_from_file(opts)
-    opts = opts or {}
+--   function Lib.make_entry.gen_from_file(opts)
+--     opts = opts or {}
 
-    local cwd = vim.fn.expand(opts.cwd or vim.fn.getcwd())
+--     local cwd = vim.fn.expand(opts.cwd or vim.fn.getcwd())
 
-    local path_display = opts.path_display
+--     local path_display = opts.path_display
 
-    local mt_file_entry = {}
+--     local mt_file_entry = {}
 
-    mt_file_entry.cwd = cwd
-    mt_file_entry.display = function(entry)
-      -- Revert session files back to paths for easier reading
-      -- This affects the display only, a selection still uses the correct file name.
-      local is_win32 = vim.fn.has('win32') == Lib._VIM_TRUE
-      local entry_value = entry.value:gsub("%%", "/")
-      local display = is_win32 and path.make_relative(entry_value:gsub("%++", ":"), cwd) or path.make_relative(entry_value, cwd)
+--     mt_file_entry.cwd = cwd
+--     mt_file_entry.display = function(entry)
+--       -- Revert session files back to paths for easier reading
+--       -- This affects the display only, a selection still uses the correct file name.
+--       local is_win32 = vim.fn.has('win32') == Lib._VIM_TRUE
+--       local entry_value = entry.value:gsub("%%", "/")
+--       local display = is_win32 and path.make_relative(entry_value:gsub("%++", ":"), cwd) or path.make_relative(entry_value, cwd)
 
-      -- there problem with telescope path_shorten() for windows
-      -- see https://github.com/nvim-telescope/telescope.nvim/issues/706
-      if vim.tbl_contains(path_display, "shorten") and not is_win32 then
-        display = utils.path_shorten(display)
-      end
+--       -- there problem with telescope path_shorten() for windows
+--       -- see https://github.com/nvim-telescope/telescope.nvim/issues/706
+--       if vim.tbl_contains(path_display, "shorten") and not is_win32 then
+--         display = path.shorten(display)
+--       end
 
-      -- Strip file extensions since sessions always have the same .vim extension.
-      local filename_only = display:match("(.+)%..+")
+--       -- Strip file extensions since sessions always have the same .vim extension.
+--       local filename_only = display:match("(.+)%..+")
 
-      return filename_only
-    end
+--       return filename_only
+--     end
 
-    -- No idea what this block does, got it from the telescope code, may or may not figure it out later.
-    mt_file_entry.__index = function(t, k)
-      local raw = rawget(mt_file_entry, k)
-      if raw then return raw end
+--     -- No idea what this block does, got it from the telescope code, may or may not figure it out later.
+--     mt_file_entry.__index = function(t, k)
+--       local raw = rawget(mt_file_entry, k)
+--       if raw then return raw end
 
-      if k == "path" then
-        local retpath = t.cwd .. path.separator .. t.value
-        if not vim.loop.fs_access(retpath, "R", nil) then
-          retpath = t.value
-        end
-        return retpath
-      end
+--       if k == "path" then
+--         local retpath = t.cwd .. path.separator .. t.value
+--         if not vim.loop.fs_access(retpath, "R", nil) then
+--           retpath = t.value
+--         end
+--         return retpath
+--       end
 
-      return rawget(t, rawget(lookup_keys, k))
-    end
+--       return rawget(t, rawget(lookup_keys, k))
+--     end
 
-    return function(line)
-      return setmetatable({line}, mt_file_entry)
-    end
-  end
-end
+--     return function(line)
+--       return setmetatable({line}, mt_file_entry)
+--     end
+--   end
+-- end
 -- ===================================================================================
 
 -- Logger =========================================================
