b88c264ab4010aa5ff25d3aa366f76b6338a4783
William Bille Meyling <22810553+willthbill@users.noreply.github.com>
2022-03-04 20:34:52 +0100

    Fix telescope timing problems (folding) (#20)

--- lua/telescope/_extensions/session-lens/session-lens-actions.lua
+++ lua/telescope/_extensions/session-lens/session-lens-actions.lua
@@ -7,16 +7,20 @@ local SessionLensActions = {}
 SessionLensActions.source_session = function(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   actions.close(prompt_bufnr)
-  AutoSession.AutoSaveSession()
-  vim.cmd("%bd!")
-  AutoSession.RestoreSession(selection.path)
+  vim.defer_fn(function ()
+    AutoSession.AutoSaveSession()
+    vim.cmd("%bd!")
+    AutoSession.RestoreSession(selection.path)
+  end, 50)
 end
 
 -- TODO: make this refresh the picker and not close it instead
 SessionLensActions.delete_session = function(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   actions.close(prompt_bufnr)
-  AutoSession.DeleteSession(selection.path)
+  vim.defer_fn(function ()
+    AutoSession.DeleteSession(selection.path)
+  end, 50)
 end
 
 return SessionLensActions
